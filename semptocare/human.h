0afc6ea784087ce0e0ea34aff7f1eb6d34b4b6b9
Mustafa Berk Keskin <116215851+MberkKeskin@users.noreply.github.com>
2023-12-07 22:58:46 +0300

    Add files via upload

---
+++ semptocare/human.h
@@ -0,0 +1,83 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class humanBody {
+private:
+
+	string bodyPart;
+	struct head {
+		struct eye {
+
+		};
+		struct mouth {
+
+		};
+		
+		struct brain {
+
+		};
+	};
+
+	struct body {
+		struct chest {
+
+		};
+
+		struct heart {
+
+
+		};
+
+		struct abdomen {
+
+		};
+	};
+
+	struct limb { 
+		struct arms {
+			struct hands {
+
+			};
+			
+			struct shoulders {
+
+			};
+		};
+
+		struct legs {
+			struct foot {
+
+			};
+
+			struct calf {
+
+			};
+		};
+
+	};
+
+	
+
+public:
+
+};
+
+class user {
+private:
+	string name;
+	int age;
+	string gender;
+	float weight;
+	float height;
+	int activityRate;
+	bool smoking;
+	bool alcholic;
+	bool alergic;
+	bool drug;
+
+public:
+	
+};
