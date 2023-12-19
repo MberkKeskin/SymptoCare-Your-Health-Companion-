aecb2c998408cf9c96be13e66a17ec39e345aa8f
Mustafa Berk Keskin <116215851+MberkKeskin@users.noreply.github.com>
2023-12-19 13:32:24 +0300

    türkçe dili eklendi ve bazı hatalar düzeltildi

--- semptocare/Kaynak.cpp
+++ semptocare/Kaynak.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 #include "human.h"
+#include <locale.h>
 
 using namespace std;
 
 int main() {
-	int choiceForEmengrcy=0,choiceForMenu=0;
+	setlocale(LC_ALL, "Turkish");
+
+	int choiceForMenu=0;
+	bool choiceForEmengrcy;
+	
 	cout<< " Welcome to Semptocare!"<<endl;
+	
 	cout<< " Do you have emengcy stitions"<<endl;
 	cin>>choiceForEmengrcy;
-	if(choiceForEmengrcy==1){
+	
+	if(choiceForEmengrcy){
 		//ACIL DURUM ICIN DIREK YONLENDIRME YAPALIM
 	}
-	else if(choiceForMenu==0){
+	else {
 		while(choiceForMenu!=2){
 		
 		cout<< " 1.Enter Patient infirmation"<<endl;
 		cout<< " 2.Exit"<<endl;
+		
 		cout<< " Enter what do you want"<<endl;
 		cin>> choiceForMenu;
 		
