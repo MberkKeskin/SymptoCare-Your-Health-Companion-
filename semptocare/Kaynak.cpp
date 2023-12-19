8a7dbd5450340852fb520a2eb481edf355ca50ac
Mustafa Berk Keskin <116215851+MberkKeskin@users.noreply.github.com>
2023-12-19 13:24:22 +0300

    Menü eklendi
    her ihtimalle karsı eklenen menü

--- semptocare/Kaynak.cpp
+++ semptocare/Kaynak.cpp
@@ -4,6 +4,34 @@
 using namespace std;
 
 int main() {
+	int choiceForEmengrcy=0,choiceForMenu=0;
+	cout<< " Welcome to Semptocare!"<<endl;
+	cout<< " Do you have emengcy stitions"<<endl;
+	cin>>choiceForEmengrcy;
+	if(choiceForEmengrcy==1){
+		//ACIL DURUM ICIN DIREK YONLENDIRME YAPALIM
+	}
+	else if(choiceForMenu==0){
+		while(choiceForMenu!=2){
+		
+		cout<< " 1.Enter Patient infirmation"<<endl;
+		cout<< " 2.Exit"<<endl;
+		cout<< " Enter what do you want"<<endl;
+		cin>> choiceForMenu;
+		
+		if(choiceForMenu==1){
+			//HASTALARIN BILGILERINI GIRDIGI KISIM
+		}
+			
+		}
+	
+		
+	}
+
+	cout<<" BIZI TERCIH ETTIGINIZ ICIN TESEKKURLER"<<endl;
+	
+	
+		
 	return 0;
 
 }
