942cc399abd071269a7242a03f3b148e0ecd8253
Arda Gönül <118387233+ardagonul@users.noreply.github.com>
2023-12-23 17:13:43 +0300

    Kullanıcıdan girdi alan fonksiyon eklendi.

--- semptocare/Kaynak.cpp
+++ semptocare/Kaynak.cpp
@@ -1,45 +1,48 @@
 #include <iostream>
-#include "human.h"
+#include <string>
 #include <locale.h>
+#include "Human.h"
 
 using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Turkish");
 
-	int choiceForMenu=0;
+	user user;
+
+	int choiceForMenu = 0;
 	bool choiceForEmergency;
-	
-	cout<< " Welcome to SymptoCare!"<<endl;
-	
-	cout<< " Do you have an emergency situation?"<<endl;
-	cin>>choiceForEmergency;
-	
-	if(choiceForEmergency){
-		//ACIL DURUM ICIN DIREK YONLENDIRME YAPALIM
+
+	cout << " Welcome to SymptoCare!" << endl;
+
+	cout << " Do you have an emergency situation?" << endl;
+	cin >> choiceForEmergency;
+
+	if (choiceForEmergency) {
+		cout << "En kisa zamanda acil servise müracaat ediniz.";
 	}
 	else {
-		while(choiceForMenu!=2){
-		
-		cout<< " 1.Enter Patient information"<<endl;
-		cout<< " 2.Exit"<<endl;
-		
-		cout<< " Enter what do you want"<<endl;
-		cin>> choiceForMenu;
-		
-		if(choiceForMenu==1){
-			//HASTALARIN BILGILERINI GIRDIGI KISIM
+		while (choiceForMenu != 2) {
+
+			cout << " 1.Enter Patient information" << endl;
+			cout << " 2.Exit" << endl;
+
+			cout << " Enter what do you want" << endl;
+			cin >> choiceForMenu;
+
+			if (choiceForMenu == 1) { //HASTALARIN BILGILERINI GIRDIGI KISIM
+				user.getInfo();
+			}
+
 		}
-			
-		}
-	
-		
+
+
 	}
 
-	cout<<" Bizi tercih ettiğiniz için teşekkürler."<<endl;
-	
-	
-		
+	cout << " Bizi tercih ettiginiz için tesekkurler." << endl;
+
+
+
 	return 0;
 
 }
