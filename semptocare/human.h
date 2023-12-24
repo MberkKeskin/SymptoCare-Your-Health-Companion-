422df8f5d45de1f11deaf745159c43388244eaa9
Mustafa Berk Keskin <116215851+MberkKeskin@users.noreply.github.com>
2023-12-24 13:11:09 +0300

    eye ve mouth eklendi

--- semptocare/human.h
+++ semptocare/human.h
@@ -1,3 +1,4 @@
+
 #pragma once
 #include <iostream>
 #include <limits>
@@ -22,13 +23,129 @@ class head : public human {
 };
 
 class eye : public head, public human {
+
+private:
+	string symptonForBlurredVision, symptonForEyePain, symptomForEyeSwell, symptom;
+
 public:
-	void arda() {
-		cout << "arda";
+	
+	string blurredVision() {
+		cout << " Do you have blurred vision? (yes/no) " << endl;
+		cin >> symptonForBlurredVision;
+		return symptonForBlurredVision;
 	}
+
+	string eyePain() {
+		cout << " Do you have eye pain? (yes/no) " << endl;
+		cin >> symptonForEyePain;
+		return symptonForEyePain;
+	}
+
+	
+
+	string eyeSweell() {
+		cout << " Do you have eye sweel? (yes/no) " << endl;
+		cin >> symptomForEyeSwell;
+		return symptomForEyeSwell;
+	}
+
+	string nothingSymptom() {
+		cout << " Do you have other symptom? (yes/no) " << endl;
+		cin >> symptom;
+		return symptom;
+	}
+
+	void findEyeDisaster(string semptomBluredVision, string semptomeyePain ,string semptomEyeSwell,string symptom) {
+		if ((semptomBluredVision == "yes" || semptomBluredVision == "Yes" || semptomBluredVision == "YES") && (semptomeyePain == "yes" || semptomeyePain == "YES" || semptomeyePain == "Yes")) {
+			cout << " You have Glokom disaster " << endl;
+			//ABİ NORMALDE BURAYI TEXT FİLE RAPOR OLARAK YAPCAZ BEN SADECE GEÇİÇİ YAZDIM
+		}
+		else if (semptomBluredVision == "yes" && semptomeyePain == "yes") {
+			cout << " You have katarakt disaster " << endl;
+			//ABİ NORMALDE BURAYI TEXT FİLE RAPOR OLARAK YAPCAZ BEN SADECE GEÇİÇİ YAZDIM
+		}
+		else if (semptomEyeSwell == "yes") {
+			cout << " You have Alerjic Konjonktivit disaster " << endl;
+			//ABİ NORMALDE BURAYI TEXT FİLE RAPOR OLARAK YAPCAZ BEN SADECE GEÇİÇİ YAZDIM
+		}
+		else if (symptom=="yes"){
+			cout << " If you have other symptoms please go to doctor again " << endl;
+			
+		}
+		else if(symptom == "no"){
+			cout << " plese enter ture department eye departmennt maybe wrong for you " << endl;
+		}
+
+	}
+
 };
 
 class mouth : public head, public human {
+private:
+	string symptonTasteDiasater,symptomBleedingTheet,symptomForToohLosse, symptomMouthDryness,symptonMouthCankerSores,semptom;
+
+public:
+	
+	string tasteDisaster() {
+		cout << " Do you have tasteDisaster? (yes/no) " << endl;
+		cin >> symptonTasteDiasater;
+		return symptonTasteDiasater;
+
+	}
+
+	string bleedingTeeth() {
+		cout << " Do you have bleeding teeth ? (yes/no) " << endl;
+		cin >> symptomBleedingTheet;
+		return symptomBleedingTheet;
+	}
+
+	string toothLosse() {
+		cout << " Do you have any tooth losse ? (yes/no) " << endl;
+		cin >> symptomForToohLosse;
+		return symptomForToohLosse;
+	}
+
+	string mouthDryness() {
+		cout << " Do you have mouth Dryness ? (yes/no) " << endl;
+		cin >> symptomMouthDryness;
+		return symptomMouthDryness;
+	}
+
+	string mouthCankerSores() {
+		cout << " Do you have mouthCankerSores ? (yes/no) " << endl;
+		cin >> symptonMouthCankerSores;
+		return symptonMouthCankerSores;
+	}
+
+	string nothingSymptom() {
+		cout << " Do you have other Symptoms ? (yes/no) " << endl;
+		cin >> semptom;
+		return semptom;
+	}
+
+	void findMouthDisaster(string semptomTasteDisaster, string semptomBleddingTeeth, string semptomToothLose, string semptomDryness, string symptomCankerSores, string semtom) {
+		if ((semptomTasteDisaster == "yes" || semptomTasteDisaster == "Yes" || semptomTasteDisaster == "YES") && (semptomBleddingTeeth == "YES" || semptomBleddingTeeth == "Yes" || semptomBleddingTeeth == "yes")) {
+			cout << "You have tongue cancer";
+		}
+		else if (symptomCankerSores == "yes" || symptomCankerSores == "YES" || symptomCankerSores == "Yes") {
+			cout << "You have cankerSores";
+		}
+		else if ((semptomDryness == "yes" || semptomDryness == "Yes" || semptomDryness == "YES") && (semptomToothLose == "yes" || semptomToothLose == "Yes" || semptomToothLose == "YES")) {
+			cout << "You have frengi";
+		}
+
+		else if (semtom == "yes" || semtom == "Yes" || semtom == "YES") {
+			cout << " You have other mouth disaster " << endl;
+		}
+
+		else if (semtom == "no" || semtom == "No" || semtom == "NO") {
+			cout << " Please enter true apartment  " << endl;
+		}
+
+	}
+
+	
+
 
 };
 
@@ -111,7 +228,7 @@ public:
 			cin.clear();
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
 			cout << "Invalid input. Please enter a valid age." << endl;
-		}	
+		}
 
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
@@ -148,12 +265,12 @@ public:
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
 			cout << "Invalid input. Please enter a valid weight" << endl;
 		}
-		
+
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
 		while (true) {
 			cout << "Are you a smoker? (yes/no): ";
-			getline(cin,habit);
+			getline(cin, habit);
 			if (habit == "yes" || habit == "no") {
 				smoking = (habit == "yes");
 				break;
@@ -165,7 +282,7 @@ public:
 
 		while (true) {
 			cout << "Do you drink occasionally? (yes/no): ";
-			getline(cin,habit);
+			getline(cin, habit);
 			if (habit == "yes" || habit == "no") {
 				drinking = (habit == "yes");
 				break;
@@ -177,7 +294,7 @@ public:
 
 		while (true) {
 			cout << "Are you an allergic person? (yes/no): ";
-			getline(cin,habit);
+			getline(cin, habit);
 			if (habit == "yes" || habit == "no") {
 				allergic = (habit == "yes");
 				break;
@@ -189,7 +306,7 @@ public:
 
 		while (true) {
 			cout << "Are you taking any medications (yes/no): ";
-			getline(cin,habit);
+			getline(cin, habit);
 			if (habit == "yes" || habit == "no") {
 				medication = (habit == "yes");
 				break;
