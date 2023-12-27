6dbe653cdcc5a8b6a08ed37b41d6e879334320a6
Mustafa Berk Keskin <116215851+MberkKeskin@users.noreply.github.com>
2023-12-27 22:59:32 +0300

    Update Kaynak.cpp

--- semptocare/Kaynak.cpp
+++ semptocare/Kaynak.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
-#include <locale.h>
-#include  "Human.h"
+
+#include  "Üst Bilgi.h"
 
 using namespace std;
 
@@ -10,8 +10,9 @@ void findBodyParts(string part) {
     human human1;
 
     if (part == "eye") {
-        string symptom1, symptom2, symptom3, symptom4;
+        string symptom1, symptom2, symptom3, symptom4,symptom6;
         eye* myEye = new eye;
+        symptom6 = myEye->getBodyPart();
         symptom1 = myEye->blurredVision();
         symptom2 = myEye->eyePain();
         symptom3 = myEye->eyeSweell();
@@ -23,8 +24,9 @@ void findBodyParts(string part) {
     }
 
     else if (part == "mouth") {
-        string symptom1, symptom2, symptom3, symptom4, symptom5, symptom6;
+        string symptom1, symptom2, symptom3, symptom4, symptom5, symptom6,symptom7;
         mouth* myMouth = new mouth;
+        symptom7 = myMouth->getBodyPart();
         symptom1 = myMouth->tasteDisaster();
         symptom2 = myMouth->bleedingTeeth();
         symptom3 = myMouth->toothLosse();
@@ -38,8 +40,9 @@ void findBodyParts(string part) {
     }
 
     else if (part == "brain") {
-        string symptom1, symptom2, symptom3, symptom4, symptom5;
+        string symptom1, symptom2, symptom3, symptom4, symptom5,symptom6;
         brain* myBrain = new brain;
+        symptom6=myBrain->getBodyPart();
         symptom1 = myBrain->headacheSymptom();
         symptom2 = myBrain->seizureSymptom();
         symptom3 = myBrain->memoryLossSymptom();
@@ -53,8 +56,9 @@ void findBodyParts(string part) {
     }
 
     else if (part == "chest") {
-        string symptom1, symptom2, symptom3, symptom4, symptom5;
+        string symptom1, symptom2, symptom3, symptom4, symptom5,symptom6;
         chest* myChest = new chest;
+        symptom6=myChest->getBodyPart();
         symptom1 = myChest->chestPain();
         symptom2 = myChest->shortnessOfBreath();
         symptom3 = myChest->cough();
@@ -68,8 +72,9 @@ void findBodyParts(string part) {
     }
 
     else if (part == "heart") {
-        string symptom1, symptom2, symptom3, symptom4, symptom5;
+        string symptom1, symptom2, symptom3, symptom4, symptom5, symptom6;
         heart* myHeart = new heart;
+        symptom6 = myHeart->getBodyPart();
         symptom1 = myHeart->chestPpain();
         symptom2 = myHeart->shortnessOfBbreath();
         symptom3 = myHeart->palpitations();
@@ -83,8 +88,9 @@ void findBodyParts(string part) {
     }
 
     else if (part == "abdomen") {
-        string symptom1, symptom2, symptom3, symptom4, symptom5;
+        string symptom1, symptom2, symptom3, symptom4, symptom5,symptom6;
         abdomen* myAbdomen = new abdomen;
+        symptom6= myAbdomen->getBodyPart();
         symptom1 = myAbdomen->abdominalPain();
         symptom2 = myAbdomen->nausea();
         symptom3 = myAbdomen->vomiting();
@@ -98,8 +104,9 @@ void findBodyParts(string part) {
     }
 
     else if (part == "arms") {
-        string symptom1, symptom2, symptom3, symptom4, symptom5;
+        string symptom1, symptom2, symptom3, symptom4, symptom5, symptom6;
         arms* myArms = new arms;
+        symptom6= myArms->getBodyPart();
         symptom1 = myArms->armPain();
         symptom2 = myArms->armSwelling();
         symptom3 = myArms->armWeakness();
@@ -113,8 +120,9 @@ void findBodyParts(string part) {
     }
 
     else if (part == "hands") {
-        string symptom1, symptom2, symptom3, symptom4, symptom5;
+        string symptom1, symptom2, symptom3, symptom4, symptom5,symptom6;
         hands* myHands = new hands;
+        symptom6= myHands->getBodyPart();
         symptom1 = myHands->handPain();
         symptom2 = myHands->handSwelling();
         symptom3 = myHands->handWeakness();
@@ -182,7 +190,7 @@ void findBodyParts(string part) {
 // ACİL SERVİS PROTOKOLÜ TANIMLANACAK
 
 int main() {
-    setlocale(LC_ALL, "Turkish");
+    
     cout << "ACİL SERVİS PROTOKOLÜ TANIMLANACAK!!!!!!!!!!!!!!!!!!!!!!!!!!!";
     user user;
     string part;
@@ -190,52 +198,52 @@ int main() {
     human insan1;
     part = insan1.getBodyPart();
     findBodyParts(part);
+    
+    /*string choiceForEmergency;
+    int choiceForMenu = 0;
 
-     string choiceForEmergency;
-     int choiceForMenu = 0;
+    cout << " Welcome to SymptoCare!" << endl;
 
-     cout << " Welcome to SymptoCare!" << endl;
+    while (true) {
+        cout << " Do you have an emergency situation? (yes/no)" << endl;
+        cin >> choiceForEmergency;
 
-     while (true) {
-         cout << " Do you have an emergency situation? (yes/no)" << endl;
-         cin >> choiceForEmergency;
+        if (choiceForEmergency == "yes") {
+            cout << "En kisa zamanda acil servise müracaat ediniz." << endl;
+            return 0;
+        }
+        else if (choiceForEmergency == "no") {
+            break;
+        }
+        else {
+            cout << "Invalid input. Please enter 'yes' or 'no'." << endl;
+        }
+    }
 
-         if (choiceForEmergency == "yes") {
-             cout << "En kisa zamanda acil servise müracaat ediniz." << endl;
-             return 0;
-         }
-         else if (choiceForEmergency == "no") {
-             break;
-         }
-         else {
-             cout << "Invalid input. Please enter 'yes' or 'no'." << endl;
-         }
-     }
+    while (choiceForMenu != 2) {
+        cout << " 1. Enter Patient information" << endl;
+        cout << " 2. Exit" << endl;
 
-     while (choiceForMenu != 2) {
-         cout << " 1. Enter Patient information" << endl;
-         cout << " 2. Exit" << endl;
+        cout << " Enter what do you want" << endl;
+        cin >> choiceForMenu;
 
-         cout << " Enter what do you want" << endl;
-         cin >> choiceForMenu;
+        if (cin.fail()) {
+            cout << "Invalid input. Please enter a valid number." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
 
-         if (cin.fail()) {
-             cout << "Invalid input. Please enter a valid number." << endl;
-             cin.clear();
-             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-             continue;
-         }
-
-         if (choiceForMenu == 1) {
-             user.getInfo();
-             part = insan1.getBodyPart();
-             findBodyParts(part);
-         }
-         else if (choiceForMenu != 2) {
-             cout << "Invalid input. Please enter 1 or 2." << endl;
-         }
-     }
-
+        if (choiceForMenu == 1) {
+            user.getInfo();
+            part = insan1.getBodyPart();
+            findBodyParts(part);
+        }
+        else if (choiceForMenu != 2) {
+            cout << "Invalid input. Please enter 1 or 2." << endl;
+        }
+    }
+    */
     cout << " Bizi tercih ettiginiz için tesekkür ederiz." << endl;
 
     return 0;
